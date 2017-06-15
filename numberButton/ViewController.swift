898aa878927e8a5ac563c0cba49e4940001cac73
Mike Land <mikepland@gmail.com>
2017-06-15 00:52:13 -0500

    adds button, two labels, and initial random number generator

--- numberButton/ViewController.swift
+++ numberButton/ViewController.swift
@@ -10,8 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var topLabel: UILabel!
+    @IBOutlet weak var bottomLabel: UILabel!
+    @IBAction func buttonIsTapped(_ sender: Any) {
+        let diceRoll = Int(arc4random_uniform(6) + 1)
+        self.bottomLabel.text = "Your number is \(diceRoll)"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        let diceRoll = Int(arc4random_uniform(6) + 1)
+        
+        self.topLabel.numberOfLines = 0
+        
+        self.topLabel.text = "Tap Button, get a random number (1 - 6), see if your number is higher than \(diceRoll)"
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
